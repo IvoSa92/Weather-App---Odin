aa6ef24501e33f6e912eecaa8935eda0323b0ecb
IvoSa92 <ivo_sauter@web.de>
2024-03-01 20:58:33 +0100

    - finished styling - changed some classes in HTML for better linking

---
+++ script.js
@@ -0,0 +1,27 @@
+// DOM Elements
+
+// API Input Elements
+
+const locationInput = document.querySelector(".title");
+const weatherLogoInput = document.querySelector(".weather-logo");
+const weatherDescriptionInput = document.querySelector(".weather-status");
+const temperatureInput = document.querySelector(".temperature");
+
+//weather details container
+const weatherDetailsContainer = document.querySelector(
+  ".weather-details-container"
+);
+const windSpeedInput = document.querySelector(".wind-speed");
+const rainInput = document.querySelector(".rain-percentage");
+const feelingInput = document.querySelector(".feeling-celsius");
+
+//forecast container
+const dayOneInput = document.querySelector(".day-one-title");
+const dayTwoInput = document.querySelector(".day-two-title");
+const dayThreeInput = document.querySelector(".day-three-title");
+
+const conditionDayOne = document.querySelector(".condition-day-one");
+const conditionDayTwo = document.querySelector(".condition-day-two");
+const conditionDayThree = document.querySelector(".condition-day-three");
+
+const
